c28b7adddc61ecd2312938d2d10a4aa03162d7a1
Aurélie Lemaitre <aurelie.lemaitre@univ-rennes2.fr>
2022-03-01 15:08:12 +0100

    Astronautes de l'ISS

--- astronautes.py
+++ astronautes.py
@@ -4,5 +4,10 @@ url = "http://api.open-notify.org/astros.json"
 
 reponse = requests.get(url)
 contenu = reponse.json()
-print(contenu)
+# print(contenu['people'])
+for qui in contenu['people'] :
+    if qui['craft'] == "ISS":
+        print(qui["name"])
+
+
 
